abcfef70350e3ba87538b02f1890e4bd86517a62
Daniel Vargas <danielgvargas@gmail.com>
2013-11-16 17:38:52 -0200

    +main README, http-echo test

---
+++ README.md
@@ -0,0 +1,9 @@
+go-cloudwalk-samples
+====================
+
+Some integration samples in Golang 
+
+## More information at
+
+* https://www.cloudwalk.io
+* https://docs.cloudwalk.io
---
+++ http-echo/http-echo_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+        "net/http"
+        "net/http/httptest"
+        "testing"
+        "net/url"
+        "io/ioutil"
+)
+
+func TestHandler(t *testing.T) {
+        ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                handler(w, r)
+        }))
+        defer ts.Close()
+        
+	values := make(url.Values)
+        values.Set("buf", "foo")
+	values.Set("anotherVariable", "bar")
+        
+	r, _ := http.Post(ts.URL+"/?"+values.Encode(), "", nil)
+        body, _ := ioutil.ReadAll(r.Body)
+        r.Body.Close()
+
+        if string(body) != "CLOUDWALK foo bar" {
+                t.Errorf("got %s want CLOUDWALK foo bar", body)
+        }
+}
